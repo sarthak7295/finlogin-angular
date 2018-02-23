5b1c7c70ae052ca02586237455d48eee975192a6
sarthak7295 <sarthak.rawat@yahoo.com>
2018-02-23 22:35:10 +0530

    try

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  test: string = 'just a test';
 }
