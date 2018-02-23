f61ccdd42c932ccea93656bcf9c87a0c0bf65bf7
sarthak7295 <sarthak.rawat@yahoo.com>
2018-02-23 22:40:55 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path: 'login',component: LoginComponent}
+      { path: 'login', component: LoginComponent }
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
