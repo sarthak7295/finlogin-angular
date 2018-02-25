e8f77528f7a4bb64a6b4c6edacc0876a6b4ba862
sarthak7295 <sarthak.rawat@yahoo.com>
2018-02-25 21:44:01 +0530

    ds

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,16 @@
-import { Component,OnInit } from '@angular/core';
-import{ AuthService} from '../../services/auth.service'
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 
-  export class LoginComponent implements OnInit {
-    test: string = 'just a test';
-    constructor(private auth: AuthService) {}
-    ngOnInit(): void {
-      console.log(this.auth.test());
-    }
+export class LoginComponent implements OnInit {
+  test: string = this.auth.test();
+  constructor(private auth: AuthService) {}
+  ngOnInit(): void {
+    console.log(this.auth.test());
   }
-
+}
